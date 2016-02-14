93b6355caa88790cccb7e19c96700f1fb8fc094c
hermes-jr <hermes@cyllene.net>
2016-02-14 14:45:06 +0300

    Added a parameter to control file size

--- gendata.sh
+++ gendata.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Generate NUM Gigabytes of pseudorandom text split in lines
+# usage: ./gendata.sh [gigs]
+# Default data size is 1G
+
 LC_CTYPE=C
-# Generate 4Gigabytes of pseudorandom text split in lines
-cat /dev/urandom | tr -dc "[:alnum:]" | fold -w 64 | head -c $((4*1024*1024*1024)) > data.dat
+
+NUM=1
+if [ ! -z ${1} ]; then
+	NUM=${1}
+fi
+
+cat /dev/urandom | tr -dc "[:alnum:]" | fold -w 64 | head -c $(( ${NUM} * 1024 * 1024 * 1024)) > data.dat
